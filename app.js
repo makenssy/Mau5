a485fe615c7cd2fa365bd060274d746fb82089fe
makenssy <31541487+makenssy@users.noreply.github.com>
2017-11-05 19:13:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// Calling the package
+var Discord = require('discord.js');
+var bot = new Discord.Client();
 
-client.on('ready', () => {
-       console.log('Ready...');   
+// Listener Event: Message Received
+bot.on('message', message => {
+
+// Variables
+  var sender = message.author; // The person who sent the message
+  var msg = message.content.toUpperCase(); // Takes the message, and makes it all uppercase
+  var prefix = '>' // The text before commands, you can set this to what ever you want
+
+  // Ping / Pong
+  if (msg === prefix + 'PING') {
+      message.channel.send('Pong!')
+  }
 
 });
 
-client.on('message' message => {
-    if (message.content === 'ping') {
-         message.reply('pong');
-         }
+// Bot Launched
+bot.on('ready', () => {
+    console.log('Okay...') //
+
+    bot.user.setStatus('Online')
+
 
 });
 
-client.login(process.env.BOT_TOKEN);
+// User joining.
+bot.on('guildMemberAdd', member => {
+
+    console.log('User ' + member.user.username + ' has joined the server!')
+    console.log(member)
+
+    var role = member.guild.roles.find('name', 'Mau5');
+    member.addRole(role)
+
+    const embed = new Discord.RichEmbed()
+              .setDescription(`:beginner: **Bienvenido** ${member.user}!!`)
+              .setColor('RANDOM')
+
+    member.guild.channels.get('376560588144508929').send({embed});
+
+
+});
+
+// User Leaving
+bot.on('guildMemberRemove', member => {
+
+  const embed = new Discord.RichEmbed()
+            .setDescription(`${member.user} *dejó el server*`)
+
+
+  member.guild.channels.get('376560588144508929').send({embed});
+
+});
+
+bot.login(process.env.BOT_TOKEN);
