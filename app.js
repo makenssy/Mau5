07dd9e796a42e23cc1558c319500369394be5d48
makenssy <31541487+makenssy@users.noreply.github.com>
2017-11-10 13:10:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,39 @@ bot.on('message', message => {
                 .setDescription(`:ping_pong: Pong! \`\n${Math.round(bot.ping)}ms\`\ `)
             message.channel.send({ embed })};
   
+    // PURGE
+
+   if (msg.startsWith(prefix + 'PURGE')) {
+     async function purge() {
+        message.delete();
+
+        if (!message.member.roles.find("name", "bot-cmd")) {
+             message.channel.send('You need the \'bot-cmd\' role to use this command.');
+            return;
+        }  
+
+        if (isNaN(args[0])) {
+           message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>');
+
+           return;
+
+       }       
+
+        const fetched =await message.channel.fetchMessages({limit: args[0]});
+        console.log(fetched.size + ' messages found, deleting...');
+
+        // Deleting the messages 
+        message.channel.bulkDelete(fetched)
+             .catch(error => message.channel.send(`Error: ${error}`));
+
+    }
+
+    purge();
+
+
+
+  }   
+  
  }); 
 
 // Bot Launched
